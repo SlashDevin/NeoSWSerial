bd32958942c68d8ed84624742b52f31356d2e199
SRGDamia1 <sdamiano@stroudcenter.org>
2017-09-13 14:03:20 -0400

    Better testing script

--- tests/RxTest/RxTest.ino
+++ tests/RxTest/RxTest.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <NeoSWSerial.h>
 
-#define BAUD 57600
+#define BAUD 38400
 
 NeoSWSerial nss( 10, 11 );
 
@@ -22,56 +22,56 @@ void setup(){
 }
 
 void loop() {
-    // char endline = '\n';
-    // String readstr = Serial1.readStringUntil(endline);
-    // if (readstr.length() > 0)
-    // {
-    //     Serial.print("Serial1 - ");
-    //     Serial.println(readstr);
-    // }
-    // else Serial.println("Serial1 - no data");
+    char endline = '\n';
+    String readstr = Serial1.readStringUntil(endline);
+    if (readstr.length() > 0)
+    {
+        Serial.print("Serial1 - ");
+        Serial.println(readstr);
+    }
+    else Serial.println("Serial1 - no data");
+
+    String readstr2 = nss.readStringUntil(endline);
+    if (readstr2.length() > 0)
+    {
+        Serial.print("                                  NSS - ");
+        Serial.println(readstr2);
+    }
+    else Serial.println("                                  NSS - no data");
+
+    // char readSer, readNSS;
+    // readSer = Serial1.read();
+    // readNSS = nss.read();
     //
-    // String readstr2 = nss.readStringUntil(endline);
-    // if (readstr2.length() > 0)
+    // if (readSer > 0 and readNSS > 0)
     // {
-    //     Serial.print("                       NSS - ");
-    //     Serial.println(readstr2);
+    //     Serial.print(readSer);
+    //     Serial.print(" (");
+    //     Serial.print(readSer, HEX);
+    //     Serial.print(")");
+    //     Serial.print("   -   ");
+    //     Serial.print(readNSS);
+    //     Serial.print(" (");
+    //     Serial.print(readNSS, HEX);
+    //     Serial.print(")");
+    //     Serial.println();
     // }
-    // else Serial.println("                       NSS - no data");
-
-    char readSer, readNSS;
-    readSer = Serial1.read();
-    readNSS = nss.read();
-
-    if (readSer > 0 and readNSS > 0)
-    {
-        Serial.print(readSer);
-        Serial.print(" (");
-        Serial.print(readSer, HEX);
-        Serial.print(")");
-        Serial.print("   -   ");
-        Serial.print(readNSS);
-        Serial.print(" (");
-        Serial.print(readNSS, HEX);
-        Serial.print(")");
-        Serial.println();
-    }
-    else if (readSer > 0)
-    {
-        Serial.print(readSer);
-        Serial.print(" (");
-        Serial.print(readSer, HEX);
-        Serial.print(")");
-        Serial.print("   -  ? (##)");
-        Serial.println();
-    }
-    else if (readNSS > 0)
-    {
-        Serial.print("? (##)  -   ");
-        Serial.print(readNSS);
-        Serial.print(" (");
-        Serial.print(readNSS, HEX);
-        Serial.print(")");
-        Serial.println();
-    }
+    // else if (readSer > 0)
+    // {
+    //     Serial.print(readSer);
+    //     Serial.print(" (");
+    //     Serial.print(readSer, HEX);
+    //     Serial.print(")");
+    //     Serial.print("   -  ? (##)");
+    //     Serial.println();
+    // }
+    // else if (readNSS > 0)
+    // {
+    //     Serial.print("? (##)  -   ");
+    //     Serial.print(readNSS);
+    //     Serial.print(" (");
+    //     Serial.print(readNSS, HEX);
+    //     Serial.print(")");
+    //     Serial.println();
+    // }
 }
